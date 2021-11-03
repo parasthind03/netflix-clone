a0d1a55014c6bf7505caef79f3d5fe6704cf4e77
Paras <parasthind299@gmail.com>
2021-11-03 09:53:25 +0530

    opt-form added

--- src/containers/faq.js
+++ src/containers/faq.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import faqdata from '../fixtures/faqs.json';
 import { Accordion } from '../components';
+import {OptForm} from '../components';
 
 export default function FaqsContainer() {
   return (
@@ -12,6 +13,13 @@ export default function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
