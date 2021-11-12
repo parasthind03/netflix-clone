1a2bfb00cdaaeda4359573a766c23c7de934b0f6
Paras <parasthind299@gmail.com>
2021-11-12 21:33:43 +0530

    filtered fetched data based on genre

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {useContent} from '../hooks'
+import selectionFilter from '../utils/selection-filter'
 
 export default function Browse() {
   const {series} = useContent('series')
   const {films} = useContent('films')
-  console.log(series)
-  console.log(films)
+
+  const sildes = selectionFilter({series, films})
+  
   return (
     <p>
       Hello from the Browse
